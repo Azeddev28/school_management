a1a87a4dbbe4b35a9169cef628fcc5b16c115f8b
Ahsan Zahid <ahsanzahid@A006-00517.local>
2022-05-02 06:14:18 +0500

    heroku setup

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,6 +1,11 @@
 from config.settings.base import *
+import dj_database_url
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
